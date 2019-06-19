2f82cbb9115787a1a275c2ffe0c61471e50f03bc
tqvinhhung4399 <tqvinhhung4399@gmail.com>
2019-06-19 10:50:02 +0700

    Create Project

---
+++ OnlineShoesStore/OnlineShoesStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineShoesStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
