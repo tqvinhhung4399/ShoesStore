9a1bd21c832ee95b106c978058734c60caf21c1d
tqvinhhung4399 <tqvinhhung4399@gmail.com>
2019-07-08 19:32:11 +0700

    UPDATE Products Controllers...all pages need for project

---
+++ OnlineShoesStore/OnlineShoesStore/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShoesStore.Models;
+
+namespace OnlineShoesStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Category()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
