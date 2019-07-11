c2949fab5f0e7be03c895b2824351ed9a47153df
tqvinhhung4399 <tqvinhhung4399@gmail.com>
2019-07-11 09:56:34 +0700

    Admin

---
+++ OnlineShoesStore/OnlineShoesStore/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShoesStore.Models;
+
+namespace OnlineShoesStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult UserManager()
+        {
+            return View();
+        }
+    }
+}
