08e60ed50351ae02b204dd80d59a18e528e04e0b
ChienTM <ck.simple.1412@gmail.com>
2019-07-21 18:40:41 +0700

    commit

--- OnlineShoesStore/OnlineShoesStore/Consts/Consts.cs
+++ OnlineShoesStore/OnlineShoesStore/Consts/Consts.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace OnlineShoesStore.Consts
 {
     public class Consts
     {
-        public const string connectionString = "Server=database-1.clb1qxxudnhs.us-east-1.rds.amazonaws.com;Database=ShoesStoreDB;uid=admin;Password=12345678";
+        public static string connectionString;
     }
 }
